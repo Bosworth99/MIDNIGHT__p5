89eb7228a42ad5ac3fbdb24b364f079dba97d495
B99-DESKTOP\B99 <josh@joshbosworth.com>
2020-02-01 11:33:39 -0500

    midnight sketch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import './styles.css';
-// import Sketch from './sketch/default_0.1.0';
-// import Sketch from './sketch/iorte_0.1.0';
-import Sketch from './sketch/iorte_0.1.2';
-// import Sketch from './sketch/shatterline_0.1.0';
+import Sketch from './sketch/midnight';
 
 class App {
 
